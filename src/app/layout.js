9b5de64a06587ee1bee30664fb3c6ab893c0556a
code8la888 <kazuhaha1997@gmail.com>
2025-04-04 16:28:08 +0800

    refactor： 調整專案結構，將共用組件存放在 _components 資料夾

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
-import Navigation from "./components/Navigation";
+import Logo from "./_components/Logo";
+import Navigation from "./_components/Navigation";
 
 // metadata 可以設定頁面的資訊，像是標題、描述、社群分享資訊等，這些資料會被 Next.js 自動插入到 <head> 區塊
 export const metadata = {
@@ -9,6 +10,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <Logo />
         <Navigation />
         <main>{children}</main>
         {/* app 目錄下的 layout 作為所有子頁面的共同布局 */}
--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,3 @@
-import Counter from "./components/Counter";
-export default async function Home() {
-  // 在 Server Component 中可以直接使用 async function 來獲取資料，
-  // Client Component 不能用 async function 當組件，需改用 useEffect + useState
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  const data = await res.json();
-
-  return (
-    <div>
-      <ul>
-        {data.map((user) => {
-          return <li key={user.id}>{user.name}</li>;
-        })}
-      </ul>
-      {/* 可以透過 props 將獲取到的資料從 Server Component 傳遞給 Client Component */}
-      <Counter users={data} />
-    </div>
-  );
+export default function Home() {
+  return <h1>The Wild Oasis, Welcome to Paradise.</h1>;
 }
